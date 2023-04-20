4585abe420c0e36ccb2857d8cf82acd1d942d819
吴坤键 <12820804+wu-kunjian@user.noreply.gitee.com>
2023-04-20 09:52:30 +0800

    完成员工类

--- Staff_Manager_Systerm/manager.cpp
+++ Staff_Manager_Systerm/manager.cpp
@@ -1,1 +1,63 @@
 #include "manager.h"
+Manager::Manager(int id, string name, int did) {
+	this->m_Id = id;
+	this->m_Name = name;
+	this->m_DeptId = did;
+}
+Manager::Manager(int id, string name, int did, int salary) {
+	this->m_Id = id;
+	this->m_Name = name;
+	this->m_DeptId = did;
+	this->m_Salary = salary;
+	int j;
+	this->m_Performance = 0;
+	for (j = 0; j < 31; j++)
+	{
+		this->m_Check_Attendance[j] = 1;
+	}
+}
+
+
+
+
+Manager::Manager(int id, string name, int did, int salary, int performance, int a[], int n) {
+	this->m_Id = id;
+	this->m_Name = name;
+	this->m_DeptId = did;
+	this->m_Salary = salary;
+	this->m_day = n;
+	int j;
+	this->m_Performance = performance;
+	for (j = 0; j < 31; j++)
+	{
+		this->m_Check_Attendance[j] = 0;
+	}
+	for (j = 0; j < n; j++)
+	{
+		this->m_Check_Attendance[j] = a[j];
+	}
+}
+
+
+void Manager::showInfo() {
+	int i;                          //j是状态变量，用于判断是否全勤
+	cout << "职工编号：" << this->m_Id
+		<< "\t职工姓名：" << this->m_Name
+		<< "\t岗位：" << this->getDeptName()
+		<< "\t薪资：" << this->get_m_Salary()
+		<< "\t岗位职责：完成经历交付的各项任务" << endl;
+	cout << "本月出勤情况：" << endl;
+	if (!this->m_Check_Attendance[0])
+		cout << "本月" << m_Name << "全勤" << endl;
+	else
+		for (i = 0; i < 31 && m_Check_Attendance[i]; i++)
+			cout << "在" << m_Check_Attendance[i] << "日，缺勤" << endl;
+
+
+	cout << "本月绩效：" << this->m_Performance << endl;
+}
+
+
+string Manager::getDeptName() {
+	return string("经理");
+}
--- Staff_Manager_Systerm/workerbase.cpp
+++ Staff_Manager_Systerm/workerbase.cpp
@@ -1,1 +1,31 @@
-#include "workerbase.h"
+#include "WorkerBase.h"
+#include <iostream>
+using namespace std;
+int WorkerBase::get_m_Id()
+{
+	return m_Id;
+};
+string WorkerBase::get_m_Name()
+{
+	return m_Name;
+};
+int WorkerBase::get_m_DeptId()
+{
+	return m_DeptId;
+};
+int WorkerBase::get_m_day()
+{
+	return m_day;
+}
+int WorkerBase::get_m_Check_Attendance(int day)
+{
+	return m_Check_Attendance[day];
+};
+int WorkerBase::get_m_Performance()
+{
+	return m_Performance;
+};
+int WorkerBase::get_m_Salary()
+{
+	return m_Salary;
+};
